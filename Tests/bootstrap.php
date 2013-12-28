caabae802b95125a52ee2f8217f13ce47ec7a0ec
Matthias Noback <matthiasnoback@gmail.com>
2013-12-28 14:34:22 +0100

    Fix problem with annotation auto-loading
    Somehow annotations could not be autoloaded. I found a simple solution
    for it.
    
    [skip ci]

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,10 +1,9 @@
 <?php
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
-use Noback\DoctrineOrmValueObject\Annotation\ValueObject;
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 
-AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
-
-$valueObject = new ValueObject(array());
+// Somehow annotations could not be auto-loaded correctly because of the package's "target-dir".
+// I found this "hack" here: https://github.com/composer/composer/issues/925
+AnnotationRegistry::registerLoader('class_exists');
